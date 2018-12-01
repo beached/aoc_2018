129e840e5a775e1df9d33bd61248d9a24223fb29
Darrell Wright <dwright-git@dawdevel.ca>
2018-12-01 01:13:06 -0500

    .

--- include/day_01.h
+++ include/day_01.h
@@ -3,14 +3,14 @@
 // Copyright (c) 2018 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files( the "Software" ), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
+// of this software and associated documentation files( the "Software" ), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and / or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -23,32 +23,29 @@
 #pragma once
 
 #include <cstdint>
-#include <unordered_map>
-#include <vector>
+#include <unordered_set>
 
 #include <daw/daw_algorithm.h>
 
 namespace daw {
 	constexpr intmax_t sum_values( std::initializer_list<intmax_t> values ) {
-		return daw::algorithm::accumulate( values.begin( ), values.end( ), static_cast<intmax_t>( 0 ) );
+		return daw::algorithm::accumulate( values.begin( ), values.end( ),
+		                                   static_cast<intmax_t>( 0 ) );
 	}
 
 	template<typename Container>
-	intmax_t find_dups( Container && values ) {
-		std::unordered_map<intmax_t, size_t> sum_count{};
+	intmax_t find_dups( Container &&values ) {
+		std::unordered_set<intmax_t> sums{};
 		intmax_t sum = 0;
-		sum_count[sum] = 1;
+		sums.insert( sum );
 		while( true ) {
-			for (auto i: values) {
+			for( auto i : values ) {
 				sum += i;
-				if (sum_count[sum] > 0) {
+				if( sums.count( sum ) > 0 ) {
 					return sum;
 				}
-				++sum_count[sum];
+				sums.insert( sum );
 			}
 		}
-		throw std::exception{};
 	}
-}
-
-
+} // namespace daw
