fe494a442ef5213b663635401753774b8097a005
Darrell Wright <dwright-git@dawdevel.ca>
2018-12-03 00:52:06 -0500

    .

--- include/day_03.h
+++ include/day_03.h
@@ -29,8 +29,90 @@
 #include <unordered_map>
 
 #include <daw/daw_algorithm.h>
+#include <daw/daw_string_view.h>
 #include <daw/daw_traits.h>
+#include <daw/daw_parser_helper_sv.h>
 
 namespace daw {
+	struct request_t {
+		size_t id;
+		int32_t left;
+		int32_t top;
+		int32_t width;
+		int32_t height;
+	};
+
+	template<typename Container>
+	std::vector<request_t> parse_request(Container &&requests) {
+		std::vector<request_t> result{};
+		for( daw::string_view sv : requests ) {
+			// format: #123 @ 3,2: 5x4
+			sv.remove_prefix( );
+			request_t tmp{};
+			auto sv_tmp = sv.pop_front( sv.find_first_of( ' ' ) );
+			tmp.id = daw::parser::parse_int<size_t>( sv_tmp );
+			sv.remove_prefix( sv.find_first_of( '@' ) + 2 );
+			sv_tmp = sv.pop_front( sv.find_first_of( ',' ) );
+			tmp.left = daw::parser::parse_int<int32_t>( sv_tmp );
+			sv.remove_prefix( );
+			sv_tmp = sv.pop_front( sv.find_first_of( ':' ) );
+			tmp.top = daw::parser::parse_int<int32_t>( sv_tmp );
+			sv.remove_prefix( 2 );
+			sv_tmp = sv.pop_front( sv.find_first_of( 'x' ) );
+			tmp.width = daw::parser::parse_int<int32_t>( sv_tmp );
+			sv.remove_prefix( );
+			tmp.height = daw::parser::parse_int<int32_t>( sv );
+			result.push_back( std::move( tmp ) );
+		}
+		return result;
+	}
+
+	template<typename T, intmax_t width, intmax_t height>
+	struct fabric_t {
+		std::array<T, width*height> m_values = {};
+
+		constexpr fabric_t( ) {
+			for( size_t n=0; n<m_values.size( ); ++n ) {
+				m_values[n] = T{};
+			}
+		}
+
+		constexpr T & operator( )( intmax_t x, intmax_t y ) {
+			return m_values[static_cast<size_t>( x ) + static_cast<size_t>( y ) * width];
+		}
+
+		constexpr T const & operator( )( intmax_t x, intmax_t y ) const {
+			return m_values[static_cast<size_t>( x ) + static_cast<size_t>( y ) * width];
+		}
+	};
+
+	template<typename Container>
+	auto apply_reqs_to_fabric( Container && requests ) {
+		fabric_t<std::vector<size_t>, 1000, 1000> fabric{};
+		for( auto const & req: requests ) {
+			for( auto x=req.left; x < (req.left + req.width); ++x ) {
+				for( auto y=req.top; y < (req.top + req.height); ++y ) {
+					fabric( x, y ).push_back( req.id );
+				}
+			}
+		}
+		return fabric;
+	}
+
+	template<typename Container>
+	intmax_t calc_conflicted_area( Container && str_requests ) {
+		auto reqs = parse_request( std::forward<Container>( str_requests ) );
+		auto fabric = apply_reqs_to_fabric( std::move( reqs ) );
+		intmax_t area = 0;
+		for( intmax_t x=0; x<1000; ++x ) {
+			for( intmax_t y=0; y<1000; ++y ) {
+				if( fabric( x, y ).size( ) > 1 ) {
+					++area;
+				}
+			}
+		}
+		return area;
+	}
+
 
 } // namespace daw
--- tests/day_02_test.cpp
+++ tests/day_02_test.cpp
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#define BOOST_TEST_MODULE aoc_2018_day_03
+#define BOOST_TEST_MODULE aoc_2018_day_02
 #include <daw/boost_test.h>
 
 #include <array>
@@ -36,9 +36,160 @@
 
 namespace daw {
 	namespace aoc_2018 {
-		namespace day03 {
+		namespace day02 {
 			using namespace std::string_literals;
+			static std::array<std::string, 250> const box_ids = {
+			  "oiwcdpbseqgxryfmlpktnupvza"s, "oiwddpbsuqhxryfmlgkznujvza"s,
+			  "ziwcdpbsechxrvfmlgktnujvza"s, "oiwcgpbseqhxryfmmgktnhjvza"s,
+			  "owwcdpbseqhxryfmlgktnqjvze"s, "oiscdkbseqhxrffmlgktnujvza"s,
+			  "oiwcdibseqoxrnfmlgktnujvza"s, "oiwcdpbsejhxryfmlektnujiza"s,
+			  "oewcdpbsephxryfmlgkwnujvza"s, "riwcdpbseqhxryfmlgktnujzaa"s,
+			  "omwcdpbseqwxryfmlgktnujvqa"s, "oiwcdqqneqhxryfmlgktnujvza"s,
+			  "oawcdvaseqhxryfmlgktnujvza"s, "oiwcdabseqhxcyfmlkktnujvza"s,
+			  "oiwcdpbseqhxryfmlrktrufvza"s, "oiwcdpbseqhxdyfmlgqtnujkza"s,
+			  "oiwcdpbseqhxrmfolgktnujvzy"s, "oiwcdpeseqhxnyfmlgktnejvza"s,
+			  "oiwcdpbseqhxrynmlaktlujvza"s, "oiwcdpbseqixryfmlektncjvza"s,
+			  "liwtdpbseqhxryfylgktnujvza"s, "ouwcdpbszqhxryfmlgktnajvza"s,
+			  "oiwzdpbseqhxryfmngktnujvga"s, "wiwcfpbseqhxryfmlgktnuhvza"s,
+			  "oiwcdpbselhfryfmlrktnujvza"s, "oywcdpbveqhxryfmlgktnujdza"s,
+			  "oiwcdpbsiqhxryfmqiktnujvza"s, "obwcdhbseqhxryfmlgktnujvqa"s,
+			  "oitcdpbseqhfryfmlyktnujvza"s, "oiwcdpbseqhxryfmlnutnujqza"s,
+			  "oiwcdpbseqhxnyfmlhktnujtza"s, "oiwcdpbseqhbryfmlgkunuwvza"s,
+			  "oiwcopbseqhiryfmlgktnkjvza"s, "oiwcdpsseqhxryfklrktnujvza"s,
+			  "oiwcdpsrsqhxryfmlgktnujvza"s, "oiwcdpbsyqrxryfmlgktnujvzc"s,
+			  "ojwcepbseqhxryfmlgktnujvfa"s, "oiwcdpbseqhxrlfmlgvtnujvzr"s,
+			  "oiycdpbsethsryfmlgktnujvza"s, "eiwcdpbseqwxryfmlgktnujcza"s,
+			  "oiocdpbseqhxryfxlgktaujvza"s, "qiwydpbseqhpryfmlgktnujvza"s,
+			  "ziwcdpbseqhxryfmlgktsuuvza"s, "oiwcdpbseqheryfmygxtnujvza"s,
+			  "oiwidpbseqhxryfulgktnujvzm"s, "oiscdpdseqhxryfmlgktnujvya"s,
+			  "oiwmypbseqhxryfmlgktnuxvza"s, "oizcwbbseqhxryfmlgktnujvza"s,
+			  "oiwcdpbseqpxryfmlgxfnujvza"s, "oiwpdpbscqhxryxmlgktnujvza"s,
+			  "oiwcdpbseqhxrifrlgkxnujvza"s, "oiwcdpbsrqhxrifmlgktnzjvza"s,
+			  "tiwcdpbseqhxryfmegkvjujvza"s, "oiwcddbseqhxryfingktnujvza"s,
+			  "oiwcdpbsiqhiryfmlgktnucvza"s, "oiwcipbseqhxrkfmlgktnuvvza"s,
+			  "kiwcdpbseqhxryfmlbkonujvza"s, "qiwcdhbsedhxryfmlgktnujvza"s,
+			  "siwcdpbseqhxryfmsgktnujvua"s, "oqwcdpbseqhxryfmlyktndjvza"s,
+			  "oiwcqnbseehxryfmlgktnujvza"s, "oiwcdybseqhxryfmlgbtnujvia"s,
+			  "oiwcdpbsejhxryfdlgktngjvza"s, "oawcdpbseqhxrbfmlkktnujvza"s,
+			  "oilcdpbseqhhrjfmlgktnujvza"s, "oibcdpbseqhxryfmngktnucvza"s,
+			  "niwcdpbseqhxryfmlgkuaujvza"s, "oiwcdpbseqhxryfmqgmtnujvha"s,
+			  "oiwcdpbseqhcryfxlgktnzjvza"s, "oiwcdpaseqhxryfmqgktnujvzl"s,
+			  "oiwcdpbseqhxjyfmlgkonujvzx"s, "oiwmdzbseqlxryfmlgktnujvza"s,
+			  "oiwhdpbsexhxryfmlgktnujvzw"s, "oiwctpbseqhxryfmlgktnummza"s,
+			  "oiwcdpbseqoxrydmlgktnujvoa"s, "oiwkdpvseqhxeyfmlgktnujvza"s,
+			  "oixcdpbsemhxryfmlgctnujvza"s, "oimcdpbseqhxryfmlgktnujvlr"s,
+			  "oiwcdpbseehxrywmlgktnejvza"s, "oiwcdpbseqoxhyfmlgktnujyza"s,
+			  "oiwcdpbsethxryfmlgktnrjvxa"s, "oiwcdpbxeqhxryfmlgktnrjvzb"s,
+			  "ogeadpbseqhxryfmlgktnujvza"s, "eiwcdpbseqhxryfmlgktnvuvza"s,
+			  "oiwcdpbseqhxryfmlgktnujaxv"s, "siwcdpbsuqhxryfmlgktnuavza"s,
+			  "oixcdpbseqhxryfmlgatnujvzy"s, "oiwcdpbzeghmryfmlgktnujvza"s,
+			  "oiwcdpbieqhxryfmlgktyujvzr"s, "oiwcdpbseqhxeyfhlgktngjvza"s,
+			  "oiwcdpbseqhjoyrmlgktnujvza"s, "iiwcdpbseqhxryfmwhktnujvza"s,
+			  "oixcdpbsiqhxryfmagktnujvza"s, "oiwcdpfljqhxryfmlgktnujvza"s,
+			  "oivcdpbseqhxrqfmlgktnujvca"s, "ovwcdpbseqhxzyfmlgkenujvza"s,
+			  "oiwxdpgseqhxryfmlgktnhjvza"s, "oibcdpbbeohxryfmlgktnujvza"s,
+			  "oiwcrpbseqhxrygmlgwtnujvza"s, "jiwcdpbseqkxryfmlgntnujvza"s,
+			  "oiwcdbbseqhxrywmlgktnujvra"s, "oiwcdpbteqyxoyfmlgktnujvza"s,
+			  "oiwcdjbsvqvxryfmlgktnujvza"s, "obwcdukseqhxryfmlgktnujvza"s,
+			  "oifcdpdseqhxryfmlgktnujsza"s, "oiwcdpbseqhxryfalgktnujyda"s,
+			  "oiwcwpbseqhxrnfmkgktnujvza"s, "oswcdpbsethcryfmlgktnujvza"s,
+			  "oiwcdpbieqhxryfmlgktnuoiza"s, "oiwcdibsehhxryfmzgktnujvza"s,
+			  "oisjdpbseqhxryfmfgktnujvza"s, "oiwcjpbseqkxqyfmlgktnujvza"s,
+			  "obwcdpbshqhgryfmlgktnujvza"s, "oiwcspbseqhxryxmlgktnujvzl"s,
+			  "oiwcdvbswqhxryfmlgklnujvza"s, "oiwcdhuseqhxryfmlgdtnujvza"s,
+			  "oiwcdpbkeqdxryfmlgktnujvzv"s, "oiwcdpzseqhxcyfmlgksnujvza"s,
+			  "oiwcdpbseqhxryfmbkkvnujvza"s, "qiwcdpbseqhxrnfmlgktnujvha"s,
+			  "okwcdpbseqhxryfmdgktgujvza"s, "oiwcdpbkeqhxryfmldktnujvzu"s,
+			  "oiwctpxseqhxgyfmlgktnujvza"s, "oiwcdpbseqhxrykmlgktnujita"s,
+			  "oiwcdpbseqhxryfmldktyujnza"s, "oiwcdpbszqhxrjfmlgktnuqvza"s,
+			  "oiwcdpbeeqhxrykmlgktnujrza"s, "oiwcvpbseqhxryhmlgwtnujvza"s,
+			  "oiwcdpbpeehxryfmlgktnujvzz"s, "oiwcdbbsxqhxyyfmlgktnujvza"s,
+			  "oiwkdpbseqhxryfplgktnujeza"s, "opwcdpbseqhxdyfmlgctnujvza"s,
+			  "oowcdpbseqhnryfmlgktnujvga"s, "oawzdibseqhxryfmlgktnujvza"s,
+			  "oiwcdpbfeqzxrjfmlgktnujvza"s, "oiwcdpbseqaxryfmlgkonulvza"s,
+			  "oiacdpbseqvxryfmlgktvujvza"s, "oiwudpbseqhxryfwlgktnujvka"s,
+			  "oiwcdpbjeqhxryfymgktnujvza"s, "oiwcdpbweqhxrynmlgktnujaza"s,
+			  "oiwcdpbseqdxryfclgvtnujvza"s, "oiwcdppseqhxryfmlgmtzujvza"s,
+			  "oiwcdpbseqhxrhfelektnujvza"s, "kiwcdpbsnqhxryfmegktnujvza"s,
+			  "oiwcdpbseqpxryfmlgzwnujvza"s, "eiwcdpbseqnxryfplgktnujvza"s,
+			  "oiwcdbbseqhxryfmlmktnujvha"s, "oiwcdpbseqhxryfmlgktjhjvka"s,
+			  "oiwcdpbseqhxnyfylgktnujvzs"s, "oiwcdpbbxqhxryfmzgktnujvza"s,
+			  "opwcdpbseqhfryfmlgktnujzza"s, "oiwcdpbsjqpxryfmtgktnujvza"s,
+			  "oiwcdpbseqhyqbfmlgktnujvza"s, "oxwcdpbseqhxrffmlgktiujvza"s,
+			  "oiwcdpbseqhxgyfmlgytnujvzq"s, "oiwidpbseqhxryfmlgxtnujvzd"s,
+			  "oiwcdpbshqhxryzmlpktnujvza"s, "oifcdpbseqbxryfmlgktdujvza"s,
+			  "biwcdzbseqhxtyfmlgktnujvza"s, "oiwcdpbswqhxryfmlgutnujvca"s,
+			  "xiwcdpbseqhxryxmlnktnujvza"s, "oiwcdpzseqhxryfrlgktdujvza"s,
+			  "oiwudpbseqhxryfmlgkqnurvza"s, "oiwqdpbseihiryfmlgktnujvza"s,
+			  "iiwjdpbseqhxryamlgktnujvza"s, "oiwcdplseqhqryfmmgktnujvza"s,
+			  "oiwcdppseqhxrmfmlgklnujvza"s, "oiwcdobseqhxryfmmgkttujvza"s,
+			  "odwcdpbseqhxryfmlgktnujvyk"s, "oiwcdpkseqhxrhfmlgktntjvza"s,
+			  "oiocdpbseqhxryfmlgjknujvza"s, "oiicdpbieqhxryfmlgksnujvza"s,
+			  "oiwcdpbseqhxryemlgktnujdla"s, "oiwcdpbseqdxryfmlgktsujvzt"s,
+			  "oiwcdcksnqhxryfmlgktnujvza"s, "oowcdpbseqhxryfmlgsfnujvza"s,
+			  "oawcdpbseqhxryfmljktnuevza"s, "oiwcdpbsaqhxrffmzgktnujvza"s,
+			  "oiwcipbseqhcryfmlgktnujvga"s, "oiwcdpbsewhxrbfmlgktnuuvza"s,
+			  "oiwcdpbsewhxryfmlgkunujvzc"s, "oiwcdpbseqhxryfmlgktiujkga"s,
+			  "jiwcdpbseqhxrlfmlgktnurvza"s, "tiwcdpbseqoxryfmliktnujvza"s,
+			  "oiwcdpbsenhxryfmlgkskujvza"s, "oiwcdpbseqhxvyfmlhktoujvza"s,
+			  "oiwcdpbseqhxeyfmlgmtnunvza"s, "oiwcdpbseqhxdyfmloktnujvzu"s,
+			  "oiwcdpbseqgxryfmlgkynejvza"s, "oudcdpbseqhxryfmlgktmujvza"s,
+			  "oiwcdpbseqhxryfmvgktnucvzc"s, "oiwcdpbseqhxrysalgwtnujvza"s,
+			  "odwodpbseqhgryfmlgktnujvza"s, "oiwcdpbseqheryzmlgktnujgza"s,
+			  "oiwcdpbseqhxryfalgwtnujvba"s, "oiwcdpbseqhxryfmlgtdnuhvza"s,
+			  "oiocdpbseqhxrefulgktnujvza"s, "kiwcdpbseqhxrywolgktnujvza"s,
+			  "niwcdpbseksxryfmlgktnujvza"s, "oiwcdybseqexryfalgktnujvza"s,
+			  "oiwcdpbbeqhxryamlgktnujpza"s, "oiecdqbseqhxryfmlgktnujcza"s,
+			  "oiwcdpbsqqhxlyfmlpktnujvza"s, "oiwcdpbsaqheryfmlgktnujlza"s,
+			  "oiecdpbseqhxryfmlgkknujvzz"s, "oiwcapbsdqhxryfmlgktvujvza"s,
+			  "ojwcdxbseqhxryfmlgktrujvza"s, "oiwhdpbseqvxrzfmlgktnujvza"s,
+			  "oiwcdppseqhtryfmlgktnujvzs"s, "oikcdpbsfqhxryfmdgktnujvza"s,
+			  "owwczpbseqhxryfilgktnujvza"s, "oifwdpbseqhxryfmlgktnujfza"s,
+			  "oowcdpbseqhxrprmlgktnujvza"s, "oiwcapbseqhxryfmjgktnujvze"s,
+			  "oiwcdpaseqhdrybmlgktnujvza"s, "tiwcdpbseqhxryfmlgkvjujvza"s,
+			  "xiwcdpbseqhoryfmlgktnujvqa"s, "eiwrdpbsyqhxryfmlgktnujvza"s,
+			  "oiwcdpbseqhxranmlgktnujvzt"s, "oiwcdpbseqhxryfqlgktnudaza"s,
+			  "oiwcdpbsvqhxrywmlgktnsjvza"s, "oewcdpbseqhxryfmlgkunujvma"s,
+			  "oiwcdpbseqhjrywmlgktnujvzb"s, "omwcdpbseqhxryfmlgktwujsza"s,
+			  "oiwcdpbyxqhxryfmlgrtnujvza"s, "oiwidpbseqhxryfhlgktnunvza"s,
+			  "oqwcdpbweqhxrybmlgktnujvza"s, "oiwcdqbseqhxryfrlgktnujfza"s,
+			  "oiacdpbseqhdryfmlgktaujvza"s, "oiwcdpbstqhxmyfmlgktyujvza"s,
+			  "oiwcdpbseqhxeyfclgktjujvza"s, "wiwcdpeseqhxryfmlgktnujvzx"s,
+			  "viwcdpbseqhxryfmlgvtvujvza"s, "oircdpbseqhxcyfmlgktnujvma"s,
+			  "miwcdpbseqtwryfmlgktnujvza"s, "oiwcppbseqhxcyfmlgxtnujvza"s,
+			  "giwcrpbseqhxryfmlgktnudvza"s, "onwcvpbseqhxryfmlgktnujqza"s,
+			  "oiwcipbseqhxryfmlgitnuqvza"s, "oiwcdpbseqhxryjmlgkonufvza"s,
+			  "oiwnwpbseqhxtyfmlgktnujvza"s, "oiwcypbseqhxryfmlgetnujvzv"s,
+			  "oiwcdpbseqhxryqmljktnkjvza"s, "olwcdpbseqhxryfmlgkenujvba"s,
+			  "biwcdpbseqwxrywmlgktnujvza"s, "oiwcdpbsevhmryjmlgktnujvza"s,
+			  "oiwcdpbseqhxryfmlzktnkjvzv"s, "oiwudpbseqhxrefmlgktnujvia"s,
+			  "oiicdpbseqhxryfdloktnujvza"s, "oihcjpbsxqhxryfmlgktnujvza"s,
+			};
 
-		} // namespace day03
+			size_t part_01( ) {
+				daw::do_not_optimize( box_ids );
+				size_t result;
+				daw::bench_test( "Part 1",
+				                 [&]( ) { result = get_checksum( box_ids ); } );
+				daw::do_not_optimize( result );
+				return result;
+			}
+
+			BOOST_AUTO_TEST_CASE( part_01_test ) {
+				BOOST_REQUIRE_EQUAL( part_01( ), 5880 );
+			}
+
+			std::string part_02( ) {
+				std::string result{};
+				daw::do_not_optimize( box_ids );
+				daw::bench_test( "Part 2", [&]( ) { result = get_match( box_ids ); } );
+				daw::do_not_optimize( result );
+				return result;
+			}
+
+			BOOST_AUTO_TEST_CASE( part_02_test ) {
+				BOOST_REQUIRE_EQUAL( part_02( ), "tiwcdpbseqhxryfmgkvjujvza" );
+			}
+		} // namespace day02
 	}   // namespace aoc_2018
 } // namespace daw
