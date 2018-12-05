1a43d7fad3ebaa58e442329251c67c2489cfb5f2
Darrell Wright <dwright-git@dawdevel.ca>
2018-12-05 12:52:29 -0500

    Removed corner case preventing 1 pass

--- include/day_05.h
+++ include/day_05.h
@@ -44,46 +44,39 @@ namespace daw {
 		size_t alchemical_reduction( daw::basic_string_view<CharT> sv ) noexcept {
 			bool changed = true;
 			std::basic_string<CharT> result{};
-			std::basic_string<CharT> tmp{};
 
-			while( changed ) {
-				changed = false;
-				tmp.reserve( sv.size( ) );
+			result.reserve( sv.size( ) );
 
-				CharT last_c = sv.front( );
-				auto const matcher = [&last_c]( auto c ) {
-					auto r = match( last_c, c );
-					last_c = c;
-					return r;
-				};
-				auto pos = sv.find_first_of_if( matcher, 1 );
-				while( pos != sv.npos ) {
-					 changed = true;
-					tmp += sv.pop_front( pos - 1 ).to_string( );
-					sv.remove_prefix( 2 );
-					while( !tmp.empty( ) and !sv.empty( ) and match( tmp.back( ), sv.front( ) ) ) {
-						tmp.pop_back( );
-						sv.pop_front( );
-					}
-					if( !sv.empty( ) ) {
-						last_c = sv.front();
-						pos = sv.find_first_of_if(matcher, 1);
-					} else {
-						pos = sv.npos;
-					}
+			CharT last_c = sv.front( );
+			auto const matcher = [&last_c]( auto c ) {
+				auto r = match( last_c, c );
+				last_c = c;
+				return r;
+			};
+			auto pos = sv.find_first_of_if( matcher, 1 );
+			while( pos != sv.npos ) {
+				changed = true;
+				result += sv.pop_front( pos - 1 ).to_string( );
+				sv.remove_prefix( 2 );
+				while( !result.empty( ) and !sv.empty( ) and
+				       match( result.back( ), sv.front( ) ) ) {
+					result.pop_back( );
+					sv.pop_front( );
 				}
-				if( changed ) {
-					if (!sv.empty()) {
-						tmp += sv.to_string();
-					}
-					result = std::move(tmp);
-					sv = basic_string_view<CharT>(result.data(), result.size());
-					tmp.clear();
+				if( !sv.empty( ) ) {
+					last_c = sv.front( );
+					pos = sv.find_first_of_if( matcher, 1 );
+				} else {
+					pos = sv.npos;
 				}
 			}
-			if( result.empty( ) ) {
-				return sv.size( );
+			while( !result.empty( ) and !sv.empty( ) and
+			       match( result.back( ), sv.front( ) ) ) {
+				changed = true;
+				result.pop_back( );
+				sv.pop_front( );
 			}
+			result += sv.to_string( );
 			return result.size( );
 		}
 
@@ -93,22 +86,27 @@ namespace daw {
 			constexpr auto const unit_types = []( ) constexpr {
 				std::array<char, 26> result{};
 				for( char n = 'A'; n <= 'Z'; ++n ) {
-					result[static_cast<size_t>( n-'A' )] = n;
+					result[static_cast<size_t>( n - 'A' )] = n;
 				}
 				return result;
-			}( );
+			}
+			( );
 
-			return std::accumulate( begin( unit_types ), end( unit_types ), std::numeric_limits<size_t>::max( ), [sv]( auto cur_min, auto u ) {
-				auto poly = sv.to_string();
-				poly.erase( std::remove_if( begin( poly ), end( poly ), [u]( auto c ) {
-					return std::toupper( c ) == u;
-				}), end( poly ) );
-				auto tmp = alchemical_reduction( daw::string_view( poly.data( ), poly.size( ) ) );
-				if( tmp < cur_min ) {
-					return tmp;
-				}
-				return cur_min;
-			} );
+			return std::accumulate(
+			  begin( unit_types ), end( unit_types ),
+			  std::numeric_limits<size_t>::max( ), [sv]( auto cur_min, auto u ) {
+				  auto poly = sv.to_string( );
+				  poly.erase(
+				    std::remove_if( begin( poly ), end( poly ),
+				                    [u]( auto c ) { return std::toupper( c ) == u; } ),
+				    end( poly ) );
+				  auto tmp = alchemical_reduction(
+				    daw::string_view( poly.data( ), poly.size( ) ) );
+				  if( tmp < cur_min ) {
+					  return tmp;
+				  }
+				  return cur_min;
+			  } );
 		}
 	} // namespace
 } // namespace daw
